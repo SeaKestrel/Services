acf20d699809ad9664bd6a89129a8ac6d5448eef
Jean Pantaléon <78849811+SeaKestrel@users.noreply.github.com>
2023-12-21 10:41:45 +0100

    Delete services directory

--- services/controleurs/controleurAfficher.php
+++
@@ -1,9 +0,0 @@
-<?php 
-ajouterActiviteHistorique('Affichage des services');
-
-// recupération des services
-$services = recupInstances($connexion, "Services");
-if($services == null || count($services) == 0) {
-	$message = "Aucun service n'a été trouvé.";
-}
-?>
